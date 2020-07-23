ebe3530ba2f4e93542ad3b0680c90bc84a8d1298
227Mahaman <zalkiabass.456@gmail.com>
2020-07-23 06:57:43 +0100

    Lien youtube

--- view/home_view.php
+++ view/home_view.php
@@ -481,6 +481,7 @@ ob_start();
             <div class="row">
                 <div class="col-lg-12">
                     <div class="video-text">
+                        <iframe src="https://youtu.be/jWp-x76WqeA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <a href="https://youtu.be/jWp-x76WqeA" class="play-btn video-popup"><i class="fa fa-play"></i></a>
                         <h4></h4>
                         <h2></h2>
