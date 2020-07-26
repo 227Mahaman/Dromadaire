877f7e90882d9846698064c62c55fddb0a772d33
227Mahaman <zalkiabass.456@gmail.com>
2020-07-26 19:07:26 +0100

    MAJ Destination view en Service

--- index.php
+++ index.php
@@ -57,8 +57,8 @@ if (!empty($_GET['action'])) {
         include_once('view/reservation_view.php');
     } elseif ($action == "media") { //View Media
         include_once('view/media_view.php');
-    } elseif ($action == "destination") { //View Destination Tarif
-        include_once('view/destination_view.php');
+    } elseif ($action == "service") { //View Nos services
+        include_once('view/service_view.php');
     } elseif ($action == "agence") { //View Agence
         include_once('view/agence_view.php');
     } elseif ($action == "contact") { //View Contact
