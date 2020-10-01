b28b11ba3744389ccd56f684531561c8752b182d
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-10-01 16:05:06 +0100

    probleme de responsive detecter par goolgle

--- view/login_view.php
+++ view/login_view.php
@@ -1,14 +1,13 @@
-
 <!DOCTYPE html>
 <html>
-    
+
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-    <style>
-        	/* Coded with love by Mutiullah Samim */
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+	<style>
+		/* Coded with love by Mutiullah Samim */
 		body,
 		html {
 			margin: 0;
@@ -16,6 +15,7 @@
 			height: 100%;
 			background: #ccc !important;
 		}
+
 		.user_card {
 			height: 200px;
 			width: 350px;
@@ -33,6 +33,7 @@
 			border-radius: 5px;
 
 		}
+
 		.brand_logo_container {
 			position: absolute;
 			height: 170px;
@@ -43,15 +44,18 @@
 			padding: 10px;
 			text-align: center;
 		}
+
 		.brand_logo {
 			height: 150px;
 			width: 150px;
 			border-radius: 50%;
 			border: 2px solid white;
 		}
+
 		.form_container {
 			margin-top: 100px;
 		}
+
 		.login_btn {
 			font-size: 20px;
 			margin-bottom: 10px;
@@ -59,38 +63,97 @@
 			background: #c0392b !important;
 			color: white !important;
 		}
+
 		.login_btn:focus {
 			box-shadow: none !important;
 			outline: 0px !important;
 		}
+
 		.login_container {
 			padding: 0 2rem;
 		}
+
 		.input-group-text {
 			background: #c0392b !important;
 			color: white !important;
 			border: 0 !important;
 			border-radius: 0.25rem 0 0 0.25rem !important;
 		}
+
 		.input_user,
 		.input_pass:focus {
 			box-shadow: none !important;
 			outline: 0px !important;
 		}
+
 		.custom-checkbox .custom-control-input:checked~.custom-control-label::before {
 			background-color: #c0392b !important;
 		}
-    </style>
+
+		@media only screen and (max-width: 600px) {
+			.user_card {
+				height: 400px;
+				width: 700px;
+				margin-top: auto;
+				margin-bottom: auto;
+				background: #f39c12;
+				position: relative;
+				display: flex;
+				justify-content: center;
+				flex-direction: column;
+				padding: 10px;
+				box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				-webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				-moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				border-radius: 5px;
+			}
+
+			.brand_logo {
+				height: 200px;
+				width: 200px;
+				border-radius: 50%;
+				border: 2px solid white;
+			}
+
+			.brand_logo_container {
+				position: absolute;
+				height: 220px;
+				width: 220px;
+				top: -75px;
+				border-radius: 50%;
+				background: #60a3bc;
+				padding: 10px;
+				text-align: center;
+			}
+
+			.input_user,
+			.input_pass:focus {
+				width: 400px !important;
+				height: 50px;
+				box-shadow: none !important;
+				outline: 0px !important;
+				font-size: 1.5rem;
+			}
+
+			.login_btn {
+				font-size: 2rem;
+				margin-bottom: 10px;
+				width: 100%;
+				background: #c0392b !important;
+				color: white !important;
+			}
+		}
+	</style>
 </head>
 <!--Coded with love by Mutiullah Samim-->
+
 <body>
 	<div class="container h-100">
 		<?php
-		 if (!empty($_SESSION['messages'])) {
-           
-              echo Manager::messages($_SESSION['messages'], 'alert-danger');
-            
-          }
+		if (!empty($_SESSION['messages'])) {
+
+			echo Manager::messages($_SESSION['messages'], 'alert-danger');
+		}
 		?>
 		<div class="d-flex justify-content-center h-100">
 			<div class="user_card">
@@ -107,19 +170,19 @@
 							</div>
 							<input type="tel" name="tel" class="form-control input_user" value="" placeholder="N° de téléphone">
 						</div>
-						
+
 						<!-- <div class="form-group">
 							<div class="custom-control custom-checkbox">
 								<input type="checkbox" class="custom-control-input" id="customControlInline">
 								<label class="custom-control-label" for="customControlInline">Remember me</label>
 							</div>
 						</div> -->
-							<div class="d-flex justify-content-center mt-3 login_container">
-				 	<button type="submit" class="btn login_btn">Login</button>
-				   </div>
+						<div class="d-flex justify-content-center mt-3 login_container">
+							<button type="submit" class="btn login_btn">Login</button>
+						</div>
 					</form>
 				</div>
-		
+
 				<!-- <div class="mt-4">
 					<div class="d-flex justify-content-center links">
 						Don't have an account? <a href="#" class="ml-2">Sign Up</a>
@@ -132,4 +195,5 @@
 		</div>
 	</div>
 </body>
+
 </html>
