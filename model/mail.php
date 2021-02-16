c3c8fd3015b69c50cc22f7287b28c95dd8fdba17
Komche <Adamoukomcheabdoulrazak@gmail.com>
2021-02-16 07:53:51 +0100

    mail pour admin

--- model/mail.php
+++ model/mail.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['client_reservation'])) { //Send Mail to Client
 
   try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'mail34.lwspanel.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
@@ -75,7 +75,7 @@ if (isset($_SESSION['client_reservation'])) { //Send Mail to Client
 
         try {
           //Server settings
-          $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+          $mail->SMTPDebug = SMTP::DEBUG_OFF;                      // Enable verbose debug output
           $mail->isSMTP();                                            // Send using SMTP
           $mail->Host       = 'mail34.lwspanel.com';                    // Set the SMTP server to send through
           $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
